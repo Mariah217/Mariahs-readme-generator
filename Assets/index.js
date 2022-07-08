4523395c47f7302a64b4d9478f31d61fb21332b9
Mariah <mariahjohnston217@outlook.com>
2022-07-07 20:54:35 -0400

    created layout of readme file once generated

--- Assets/index.js
+++ Assets/index.js
@@ -3,9 +3,6 @@ import inquirer from 'inquirer';
 import fs from 'fs';
 
 // TODO: Create an array of questions for user input
-const generateREADME = ({title, description, installation, usage, contributing, tests, license, github, email}) =>
-``
-
 const questions = [
     {
         type: 'input',
@@ -40,24 +37,57 @@ const questions = [
     {
         type: 'rawlist',
         message:'Select a license from the following:',
-        choices: ['MIT', 'Apache', 'GPL', 'Compliant', 'MIT + file License'],
+        choices: ['MIT Licebse', 'Apache License', 'GPL License', 'Compliant License','N/A'],
         name: 'license',
     },
     {
         type: 'input',
-        message: 'Enter your GitHub username.',
+        message: 'GitHub username:',
         name: 'github',
     },
     {
         type: 'input',
-        message: 'Enter your e-mail.',
+        message: 'E-mail address:',
         name: 'email',
     },
 ];
 
 // TODO: Create a function to write README file
-const filename = response + "README.md"
-fs.writeFileSync("README.md", generateREADME(response))
+const filename = response + "README.md";
+fs.writeFileSync(filename, JSON.stringify(response,null))
+
+const generateREADME = `# ${title}
+#Description:
+${description}
+
+#Table of Contents: 
+*[Description](#description)
+*[Installation](#installation)
+*[Usage](#usage)
+*[Contributions](#contributions)
+*[Tests](#tests)
+*[License](#license)
+
+#Installation
+${installation}
+
+##Usage
+${usage}
+
+##Contribution
+${contributing}
+
+##Tests
+${tests}
+
+##License
+${license}
+
+#Please contact me if you have any questions!
+
+*GitHub: ${github}
+*E-mail: ${email}
+`
 
 // TODO: Create a function to initialize app
 inquirer
